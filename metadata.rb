bb56028f302bf8a2df53d849b1a66b5c2dbb02fc
nat <nat.lownes@gmail.com>
2012-03-16 22:29:32 -0400

    user name fix

--- metadata.rb
+++ metadata.rb
@@ -2,4 +2,4 @@ maintainer_email "nat.lownes@gmail.com"
 license          "DWTFYW Public License"
 description      "Installs/Configures rtorrent-cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.0.3"
